a1f8df03e2c64274006c09b02b04b20eca253c3e
NYUAD <nyuad@C3-019-IMACP-14.local>
2020-11-15 16:41:43 +0400

    adding stuff

--- November17/November17.ino
+++ November17/November17.ino
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-const int outPin = 3;
-const int inPin = 4;
-=======
+
 #include "pitches.h"
 
 const int outPin = 3;
@@ -14,34 +11,25 @@ int pitches[] = {
   NOTE_C4, NOTE_D4, NOTE_E4, NOTE_F4,
   NOTE_G4, NOTE_A4, NOTE_B4, NOTE_C5
 };
->>>>>>> 7a0304ebb8c0fa3884407e7ee5ec7dfb2a3e60e0
 
 void setup() {
   pinMode(outPin, OUTPUT);
   pinMode(inPin, INPUT);
-<<<<<<< HEAD
-=======
+
   pinMode(buttonPin, INPUT);
   pinMode(buzzPin, OUTPUT);
->>>>>>> 7a0304ebb8c0fa3884407e7ee5ec7dfb2a3e60e0
   Serial.begin(9600);
 }
 
 void loop() {
-<<<<<<< HEAD
+
   digitalWrite(outPin, LOW);
   delayMicroseconds(2);
-=======
->>>>>>> 7a0304ebb8c0fa3884407e7ee5ec7dfb2a3e60e0
+
   digitalWrite(outPin, HIGH);
   delayMicroseconds(5);
   digitalWrite(outPin, LOW);
 
-<<<<<<< HEAD
-  long duration = analogRead(inPin);
-
-  Serial.println(duration);
-=======
   // Read pulse from dist. sensor
   long d = pulseIn(inPin, HIGH);
   // Constrain duration of pulse to prevent craziness
@@ -59,5 +47,5 @@ void loop() {
   else{
     noTone(8);
   }
->>>>>>> 7a0304ebb8c0fa3884407e7ee5ec7dfb2a3e60e0
+
 }
