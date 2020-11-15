7a0304ebb8c0fa3884407e7ee5ec7dfb2a3e60e0
Matthew Varona <mjv379@nyu.edu>
2020-11-15 16:36:29 +0400

    Theremin

---
+++ November17/November17.ino
@@ -0,0 +1,44 @@
+#include "pitches.h"
+
+const int outPin = 3;
+const int inPin = 4;
+const int buttonPin = 7;
+const int buzzPin = 8;
+
+// One octave
+int pitches[] = {
+  NOTE_C4, NOTE_D4, NOTE_E4, NOTE_F4,
+  NOTE_G4, NOTE_A4, NOTE_B4, NOTE_C5
+};
+
+void setup() {
+  pinMode(outPin, OUTPUT);
+  pinMode(inPin, INPUT);
+  pinMode(buttonPin, INPUT);
+  pinMode(buzzPin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  digitalWrite(outPin, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(outPin, LOW);
+
+  // Read pulse from dist. sensor
+  long d = pulseIn(inPin, HIGH);
+  // Constrain duration of pulse to prevent craziness
+  long duration = constrain(d, 100, 2500);
+  // Map to integer value
+  int noteVal = int(map(duration, 100,2500, 0, 7));
+
+  Serial.println(duration);
+
+  // If the button is pressed, play the sound
+  if(digitalRead(buttonPin) == 1){
+    tone(buzzPin, pitches[noteVal]);
+  }
+  // Otherwise stop it
+  else{
+    noTone(8);
+  }
+}
---
+++ November17/README.md
@@ -0,0 +1,5 @@
+# November 17 - Musical Instrument
+
+I made a theremin(ish)! While the button is pressed, a note plays. The pitch of that note is determined by the distance of your hand from the distance sensor. The pitch snaps to notes within one octave of the C major scale, to avoid weird fluctuations in pitch. The instrument uses a buzzer and also has a volume knob.
+
+While the distance sensor provided in the kit is quite jumpy and unstable, it's still fun to play around with. I had to do a bunch of work (like constraining the distance variable and then mapping to C major notes) to make the theremin usable, and even then you can still notice it fluttering between notes.
